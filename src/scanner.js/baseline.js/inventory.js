5f655d4174882667628cf5872348fc5aebca5293
Fatimah Mahdaim <fatimahmahdaim@gmail.com>
2026-04-25 23:32:09 +0500

    src

---
+++ src/scanner.js/baseline.js/inventory.js
@@ -0,0 +1,15 @@
+continuous-cloud-security-assessment/
+│
+├── src/
+│   ├── scanner.js
+│   ├── baseline.js
+│   ├── inventory.js
+│
+├── docs/
+│   ├── architecture.md
+│   ├── controls.md
+│
+├── README.md
+├── DEPLOYMENT.md
+├── SECURITY.md
+├── package.json
